e001aba328b9f6a2d637f873629a31567eda7ce3
yash shokeen <yashshokeen@yashs-MacBook-Air.local>
2021-11-20 10:32:44 +0530

    corrected flow005.cpp

--- flow005.cpp
+++ flow005.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
 int n,test;
 int notes;
-vector<int> den ;
+vector<int> den;
 den.push_back(1);
 den.push_back(2);
 den.push_back(5);
 den.push_back(10);
 den.push_back(50);
 den.push_back(100);
+
 cin>>n;
 while(n--)
 {
@@ -20,16 +21,17 @@ cin>>input;
 
 while(input)
 {
-    for (int i = 4; i >= 0; i--)
+    for (int i = 5; i >= 0; i--)
     {
     while(input>=100)
     {input-=100;
     flag++;
     }
-        if(input>den[i])
+        if(input>=den[i])
         {
             input-=den[i];
             flag++;
+            break;
         }
     }
 }
