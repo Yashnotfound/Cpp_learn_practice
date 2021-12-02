2fef8227e09e419d6cfacf5b5b98b762b8524ec9
yash <yashshokeen161019@gmail.com>
2021-12-02 15:53:45 +0530

    modified circular sum

--- circular_sum.cpp
+++ circular_sum.cpp
@@ -3,70 +3,55 @@ using namespace std;
 
 int main(){
     
- int n;
- cin >> n;
+    int t;
+    cin>>t;
 
- while(n--)
- {
-    int len;
-    cin >> len;
-    int lenc= len;
-    int lenc_r =len;
-    int arr[len];
-    int temp;
-    int maximum=0;
-    int max_i;
+    while(t--)
+    {
+        int len;
+        cin>>len;
+        int lenc=len;
+        int arr[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            cin>>arr[i];
+        }
+
+        int arr1[len*2];
+        for (int i = 0, j=len; i < len; i++,j++)
+        {
+            arr1[i]=arr[i];
+            arr1[j]=arr[i];
+        }
+        
+        int temp;
+        int maximum=0;
 
     for (int i = 0; i < len; i++)
-    {
-        cin >> arr[i];
-        temp = arr[i];
-        maximum = max(temp,maximum);
-    }
+    {         
+        temp = arr1[i];
+        for (int j = i; j < len+i-1; j++)
+        {
+            temp = temp + arr1[j+1];
+            maximum = max(temp,maximum);
+        }
+        
+    }   
+
+     for (int i = len*2-1; i >= len; i--)
+     {
+         temp = arr1[i];
+         for (int j = i; j >= len+i; j--)
+         {
+             temp = temp + arr1[j-1];
+             maximum = max(temp,maximum);
+         }
+     }
+
+    cout<< maximum<<endl;
     
-    for (int i = 0; i < len; i++)
-    {
-        if(arr[i]==maximum)
-        {max_i=i;
-        break;}
-    }
-    
-    
-
-    temp = arr[max_i];
-    while(--lenc)
-    {   
-        if(max_i==len-1)
-        {max_i=-1;}
-        temp = temp + arr[max_i+1];
-        maximum = max(temp,maximum);
-        max_i++;
-    }
-
-    int max1= maximum;
-    int arr_rev[len];
-
-    for (int i = 0,j = len-1; i < len; i++,j--)
-    {
-        arr_rev[i]=arr[j];
-    }
-    int max_i2= len-max_i-1;
-
-    temp = arr[max_i2];
-    while(--lenc_r)
-    {   
-        if(max_i2==len-1)
-        {max_i2=-1;}
-        temp = temp + arr_rev[max_i2+1];
-        maximum = max(temp,maximum);
-        max_i2++;
-    }
-
-    int max2 = maximum;
-
-    cout << max(max1,max2)<<endl;
  }
-
 
     return 0;
 }
