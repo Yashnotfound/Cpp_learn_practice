3e52c12e6830495fd0ae3cef3fa10c0c1ac93276
yash <yashshokeen161019@gmail.com>
2021-12-11 15:49:12 +0530

    added vandh.cpp

--- code chef/easy/VANDH.cpp
+++ code chef/easy/VANDH.cpp
@@ -1,71 +1,62 @@
 # include <iostream>
-# include <array>
 using namespace std;
 
 int main(){
     
-    int t;
-    cin>>t;
 
-    while(t--)
+int t;
+cin>>t;
+while(t--)
+{
+
+string ans ="";
+int v,h;
+cin>>h>>v;
+
+if(h==v)
+{
+    for (int i = 0; i <= h; i++)
     {
-        int v,h;
-        cin>>v>>h;
- 
-        int maximum= max(v,h);
-        int size = (maximum*2)+1;
-        if(v==h)
-        {size=(maximum*2) +2;}
-        cout<<size<<endl;
-        int arr[size];
-        int arr1[size];
+        ans += "01";
+    }
+    
+}
 
-        for (int i = 0; i < size; i++)
-        {
-            arr[i]=0;
-        }
-        
-        for (int i = 0; i < size; i++)
-        {
-            arr1[i]=1;
-        }
-    if (v>=h)
-    { 
-            int v1=v+1;
-           for (int i = 0; i < size; i++)
-        {       if(v1 != 0)
-                {if((i+1)%2==0)
-                {arr[i]=1;
-                v1--;}
-                }
-        }
-        
-            for (auto i : arr)
-            {
-                cout<<i;
-            }
-            cout<<endl;
-            
+else if(h>v)
+{
+    for (int i = 0; i < v; i++)
+    {
+        ans += "01";
     }
 
-    else
+    for (int i = 0; i < h-v; i++)
     {
-        int h1=h+1;
-        for (int i = 0; i < size; i++)
-        {   if(h1 != 0)
-            {if((i+1)%2==0)
-            {arr1[i]=0;
-            h1--;}
-            }
-        }
-        
-        for (auto i : arr1)
-            {
-                cout<<i;
-            }
-            cout<<endl;
-    }   
-
+        ans+="010";
     }
+
+
+}
+
+
+else
+{
+    for (int i = 0; i < h+1; i++)
+    {
+        ans += "10";
+    }
+
+    for (int i = 0; i < v-h-1; i++)
+    {
+        ans += "110";
+    }
+    ans += "1";
+
+}
+cout<<ans.length()<<endl;
+cout<<ans<<endl;
+
+
+}
+
     return 0;
 }
