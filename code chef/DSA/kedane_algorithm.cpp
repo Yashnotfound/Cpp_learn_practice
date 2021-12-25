7edc0d9de2b70e81b789335b445cd64986c0ff93
yash <yashshokeen161019@gmail.com>
2021-12-25 12:49:55 +0530

    added kedane's algorithm

--- code chef/DSA/kedane_algorithm.cpp
+++ code chef/DSA/kedane_algorithm.cpp
@@ -10,24 +10,30 @@ int main(){
     {
         cin>>arr[i];
     }
-    int mx=0;
-    for (int i = 0; i < n; i++)
-    {   
-        for (int j = i; j < n; j++)
-        {   int sum=0;  
-            for (int k = i; k < j; k++)
-            {
-                sum+=arr[k];
-            }
-            mx=max(mx,sum);
-        }
+    int mx=arr[0];
+    // for (int i = 0; i < n; i++) 
+    // {   
+    //     for (int j = i; j < n; j++)
+    //     {   int sum=0;  
+    //         for (int k = i; k <= j; k++)
+    //         {
+    //             sum+=arr[k];
+    //         }
+    //         mx=max(mx,sum);
+    //     }
         
-    }
+    // }
 
+       int sum=0;
+       for (int i = 0; i < n; i++)
+       {
+           sum+=arr[i];
+           mx=max(sum,mx);
+           if(sum<0)
+           {sum=0;}
+       } 
+        
     cout<<mx<<endl;
     
-
-    
-
     return 0;
 }
